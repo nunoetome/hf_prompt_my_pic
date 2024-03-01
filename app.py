08bdb94c1413d26645084ee263a4bdc9de2a658d
Nuno Tomé <nunotomeit@gmail.com>
2024-03-01 07:35:40 +0000

    added app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import torch
+from diffusers import StableDiffusionPipeline, DDIMScheduler, AutoencoderKL
+from PIL import Image
+from ip_adapter.ip_adapter_faceid import IPAdapterFaceIDPlus
+import cv2
+from insightface.app import FaceAnalysis
+from insightface.utils import face_align
+import gradio as gr
+from huggingface_hub import hf_hub_download
+from datetime import datetime
